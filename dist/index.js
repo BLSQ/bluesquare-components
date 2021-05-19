6abc14a347411b26789690b8c68d2e8e1a8e4c7e
Quang Son Le <qle@bluesquarehub.com>
2021-05-19 15:40:32 +0200

    feat add dist folder

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "LoadingSpinner", {
+  enumerable: true,
+  get: function get() {
+    return _LoadingSpinner.LoadingSpinner;
+  }
+});
+
+var _LoadingSpinner = require("./LoadingSpinner");

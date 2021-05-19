7628fa59d66d1ff0a80bb0863c6225bed94ef399
Quang Son Le <qle@bluesquarehub.com>
2021-05-19 15:33:16 +0200

    Feat: add basic React app, export LoadingSpinner

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import  {LoadingSpinner}  from "./components/LoadingSpinner";
+
+function App() {
+    return (<div>
+        <h1>Bluesquare-Components</h1>
+        <LoadingSpinner classes={{}}/>
+    </div>)
+}
+
+export default App

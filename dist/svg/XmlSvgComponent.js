59dd7ee686f6a8afdd7f632de243a155c722081a
Quang Son Le <qle@bluesquarehub.com>
2021-05-20 17:26:32 +0200

    build move build back to dist

--- dist/svg/XmlSvgComponent.js
+++ dist/svg/XmlSvgComponent.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.DHIS2Svg = DHIS2Svg;
+exports.XmlSvg = XmlSvg;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -11,22 +11,24 @@ var _SvgIcon = _interopRequireDefault(require("@material-ui/core/SvgIcon"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function DHIS2Svg(props) {
+function XmlSvg(props) {
   var finalProps = _objectSpread(_objectSpread({}, props), {}, {
-    viewBox: '0 0 200 182'
+    viewBox: '0 0 40 40'
   });
 
   return /*#__PURE__*/_react["default"].createElement(_SvgIcon["default"], finalProps, /*#__PURE__*/_react["default"].createElement("g", null, /*#__PURE__*/_react["default"].createElement("path", {
-    d: "M191.73,60,109,6.34a19.73,19.73,0,0,0-20.32,0L8.31,58.43a12,12,0,0,0-.25,20.63L88.6,134a19.37,19.37,0,0,0,20.37.25l82.76-53.65a11.88,11.88,0,0,0,0-20.59Zm-91,61.45a4.29,4.29,0,0,1-3.49-.05l-77-52.49L97,19.13a4.76,4.76,0,0,1,3.74,0L179.6,70.28Z"
+    d: "M15.2,3L7,11.6v22.9h25.5V3H15.2z M14.5,6.6v3.8h-3.7L14.5,6.6z M30.5,32.5H9V12.4h7.4V5h14.1V32.5z"
+  }), /*#__PURE__*/_react["default"].createElement("g", null, /*#__PURE__*/_react["default"].createElement("path", {
+    d: "M12.4,28.1L10.7,31H9.4l2.4-4l-2.2-3.9h1.3l1.6,2.7l1.6-2.7h1.3L13,27l2.5,4h-1.3L12.4,28.1z"
   }), /*#__PURE__*/_react["default"].createElement("path", {
-    d: "M88.66,47.82,45.1,76.06l13.61,9.33L97,60.61a4.76,4.76,0,0,1,3.74,0l39.37,25.52,14-9.06L109,47.82A19.76,19.76,0,0,0,88.66,47.82Z"
+    d: "M16.4,23.1h1.1l2.2,5.2l2.2-5.2h1.1V31h-1.2v-4.9h0l-1.6,3.8h-1l-1.6-3.8h0V31h-1.2V23.1z"
   }), /*#__PURE__*/_react["default"].createElement("path", {
-    d: "M191.73,101.46l-8.62-5.59-14.05,9.06,10.53,6.83-78.91,51.15a4.37,4.37,0,0,1-3.49,0l-77-52.5,10-6.47L16.55,94.57,8.31,99.91a12,12,0,0,0-.25,20.63L88.6,175.46a19.34,19.34,0,0,0,20.37.24l82.75-53.65a11.88,11.88,0,0,0,0-20.59Z"
-  })));
+    d: "M25,23.1h1.2v6.8H30V31h-5V23.1z"
+  }))));
 }

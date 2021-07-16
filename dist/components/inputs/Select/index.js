209012c4e57b6c7eaf6c028b86f2172bf4f4108a
Christophe Gerard <cgerard@bluesquarehub.com>
2021-07-16 13:36:36 +0200

    autocomplete  + remove dead code

--- dist/components/inputs/Select/index.js
+++ dist/components/inputs/Select/index.js
@@ -11,25 +11,33 @@ var _react = _interopRequireWildcard(require("react"));
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _reactSelect = _interopRequireDefault(require("react-select"));
+var _TextField = _interopRequireDefault(require("@material-ui/core/TextField"));
+
+var _Box = _interopRequireDefault(require("@material-ui/core/Box"));
+
+var _Chip = _interopRequireDefault(require("@material-ui/core/Chip"));
+
+var _Autocomplete = _interopRequireDefault(require("@material-ui/lab/Autocomplete"));
 
 var _core = require("@material-ui/core");
-
-var _FormControl = require("../FormControl");
-
-var _InputLabel = require("../InputLabel");
 
 var _useSafeIntl = require("../../../utils/useSafeIntl");
 
 var _messages = require("./messages");
-
-var _styles = require("./styles");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
@@ -43,116 +51,203 @@ function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Sy
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
-var SelectComponent = function SelectComponent(_ref) {
+var useStyles = (0, _core.makeStyles)(function () {
+  return {
+    chipLabel: {
+      marginTop: -2
+    },
+    startAdornment: {
+      marginTop: -5
+    }
+  };
+});
+
+var SelectCustom = function SelectCustom(_ref) {
   var value = _ref.value,
       keyValue = _ref.keyValue,
       label = _ref.label,
       errors = _ref.errors,
-      _onChange = _ref.onChange,
+      onChange = _ref.onChange,
       options = _ref.options,
-      _onBlur = _ref.onBlur,
-      _onFocus = _ref.onFocus,
-      withMarginTop = _ref.withMarginTop,
+      touched = _ref.touched,
+      onBlur = _ref.onBlur,
       multi = _ref.multi,
       disabled = _ref.disabled,
       clearable = _ref.clearable,
-      isFocused = _ref.isFocused,
-      searchable = _ref.searchable,
       required = _ref.required,
-      classes = _ref.classes;
+      noOptionsText = _ref.noOptionsText,
+      getOptionLabel = _ref.getOptionLabel,
+      getOptionSelected = _ref.getOptionSelected,
+      loading = _ref.loading,
+      renderOption = _ref.renderOption;
 
-  var _useState = (0, _react.useState)(''),
+  var _useState = (0, _react.useState)(null),
       _useState2 = _slicedToArray(_useState, 2),
-      selectInputValue = _useState2[0],
-      setSelectInputValue = _useState2[1];
+      selectedValue = _useState2[0],
+      setSelectedValue = _useState2[1];
 
-  var hasErrors = errors.length > 0;
-  var classNames = hasErrors ? [classes.select, classes.selectError] : [classes.select];
-  var intl = (0, _useSafeIntl.useSafeIntl)();
-
-  var _useState3 = (0, _react.useState)(isFocused),
+  var _useState3 = (0, _react.useState)([]),
       _useState4 = _slicedToArray(_useState3, 2),
-      focus = _useState4[0],
-      setFocus = _useState4[1];
+      multiSelectedValue = _useState4[0],
+      setMultiSelectedValue = _useState4[1];
 
-  return /*#__PURE__*/_react["default"].createElement(_FormControl.FormControl, {
-    withMarginTop: withMarginTop,
-    errors: errors
-  }, /*#__PURE__*/_react["default"].createElement(_InputLabel.InputLabel, {
-    htmlFor: "input-select-".concat(keyValue),
-    label: label,
-    shrink: value !== undefined && value !== null || selectInputValue !== '',
-    isFocused: focus,
-    required: required,
-    error: hasErrors
-  }), /*#__PURE__*/_react["default"].createElement("div", {
-    className: classNames.join(' ')
-  }, /*#__PURE__*/_react["default"].createElement(_reactSelect["default"], {
-    disabled: disabled,
-    searchable: searchable,
-    multi: multi,
-    clearable: clearable,
-    simpleValue: true,
-    onInputChange: function onInputChange(newValue) {
-      setSelectInputValue(newValue);
-    },
-    name: keyValue,
-    value: value,
-    placeholder: "",
-    onBlur: function onBlur() {
-      setFocus(false);
+  var intl = (0, _useSafeIntl.useSafeIntl)();
+  var classes = useStyles();
 
-      _onBlur();
-    },
-    onFocus: function onFocus() {
-      setFocus(true);
+  var getOption = function getOption(optionValue) {
+    return options.find(function (o) {
+      return "".concat(o.value) === "".concat(optionValue);
+    });
+  };
 
-      _onFocus();
-    },
-    options: options,
-    noResultsText: intl.formatMessage(_messages.MESSAGES.noOptions),
-    onChange: function onChange(newValue) {
-      _onChange(newValue);
+  (0, _react.useEffect)(function () {
+    if (value) {
+      if (multi) {
+        var newSelectedValue = [];
+        var valuesList = Array.isArray(value) ? value : value.split(',');
+        valuesList.forEach(function (v) {
+          var option = getOption(v);
+          if (option) newSelectedValue.push(getOption(v));
+        });
+        setMultiSelectedValue(newSelectedValue);
+      } else {
+        var _newSelectedValue = getOption(value);
+
+        if (_newSelectedValue) setSelectedValue(_newSelectedValue);
+      }
+    } else {
+      multi ? setMultiSelectedValue([]) : setSelectedValue(null);
     }
-  })));
+  }, [value, options]);
+
+  var handleChange = function handleChange(e, newValue) {
+    if (!multi && !newValue || multi && newValue.length === 0) {
+      return onChange(null);
+    }
+
+    if (multi) {
+      return onChange(newValue.map(function (v) {
+        return v && v.value;
+      }).join(','));
+    }
+
+    return onChange(newValue.value);
+  };
+
+  var extraProps = {
+    getOptionLabel: getOptionLabel || function (option) {
+      return option && option.label;
+    },
+    getOptionSelected: getOptionSelected || function (option, val) {
+      return val && option.value === val.value;
+    }
+  };
+
+  if (renderOption) {
+    extraProps.renderOption = renderOption;
+  }
+
+  return /*#__PURE__*/_react["default"].createElement(_Box["default"], {
+    mt: 1,
+    mb: 2
+  }, /*#__PURE__*/_react["default"].createElement(_Autocomplete["default"], _extends({
+    noOptionsText: intl.formatMessage(noOptionsText),
+    multiple: multi,
+    id: keyValue,
+    disableClearable: !clearable,
+    options: options,
+    value: multi ? multiSelectedValue : selectedValue,
+    onChange: handleChange,
+    loading: loading,
+    renderTags: function renderTags(tagValue, getTagProps) {
+      return tagValue.map(function (option, index) {
+        if (!option) return null;
+        return /*#__PURE__*/_react["default"].createElement(_Chip["default"], _extends({
+          classes: {
+            label: classes.chipLabel
+          },
+          color: "secondary",
+          label: option.label
+        }, getTagProps({
+          index: index
+        })));
+      });
+    },
+    renderInput: function renderInput(params) {
+      var paramsCopy = _objectSpread({}, params);
+
+      var inputExtraProps = {};
+
+      if (extraProps.renderOption && params.inputProps.value) {
+        inputExtraProps = {
+          startAdornment: /*#__PURE__*/_react["default"].createElement("div", {
+            className: classes.startAdornment
+          }, extraProps.renderOption({
+            label: params.inputProps.value
+          })),
+          style: {
+            color: 'transparent'
+          }
+        };
+        inputExtraProps.startAdornment = /*#__PURE__*/_react["default"].createElement("div", {
+          className: classes.startAdornment
+        }, extraProps.renderOption({
+          label: params.inputProps.value
+        }));
+        paramsCopy.inputProps.value = '';
+      }
+
+      return /*#__PURE__*/_react["default"].createElement(_TextField["default"], _extends({}, paramsCopy, {
+        variant: "outlined",
+        disabled: disabled,
+        label: "".concat(label).concat(required ? '*' : ''),
+        onBlur: onBlur,
+        error: errors.length > 0 && touched,
+        InputProps: _objectSpread(_objectSpread({}, params.InputProps), {}, {
+          endAdornment: /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, loading ? /*#__PURE__*/_react["default"].createElement(_core.CircularProgress, {
+            color: "inherit",
+            size: 20
+          }) : null, params.InputProps.endAdornment)
+        }, inputExtraProps)
+      }));
+    }
+  }, extraProps)));
 };
 
-SelectComponent.defaultProps = {
+exports.Select = SelectCustom;
+SelectCustom.defaultProps = {
   value: undefined,
   errors: [],
-  withMarginTop: true,
+  label: '',
   multi: false,
   disabled: false,
   clearable: true,
-  isFocused: false,
   required: false,
-  searchable: true,
-  onChange: function onChange() {},
+  touched: false,
+  loading: false,
   options: [],
   onBlur: function onBlur() {},
-  onFocus: function onFocus() {},
-  label: '' // TODO use library translations
-  // noResultsText: '',
-
+  getOptionSelected: null,
+  getOptionLabel: null,
+  renderOption: null,
+  noOptionsText: _messages.MESSAGES.noOptions
 };
-SelectComponent.propTypes = {
-  withMarginTop: _propTypes["default"].bool,
+SelectCustom.propTypes = {
   errors: _propTypes["default"].arrayOf(_propTypes["default"].string),
   keyValue: _propTypes["default"].string.isRequired,
   label: _propTypes["default"].string,
   required: _propTypes["default"].bool,
   disabled: _propTypes["default"].bool,
-  searchable: _propTypes["default"].bool,
   clearable: _propTypes["default"].bool,
-  isFocused: _propTypes["default"].bool,
   multi: _propTypes["default"].bool,
   value: _propTypes["default"].any,
   onBlur: _propTypes["default"].func,
-  onFocus: _propTypes["default"].func,
-  // noResultsText: PropTypes.string,
+  noOptionsText: _propTypes["default"].oneOfType([_propTypes["default"].object, _propTypes["default"].string]),
   options: _propTypes["default"].array,
-  onChange: _propTypes["default"].func,
-  classes: _propTypes["default"].object.isRequired
+  touched: _propTypes["default"].oneOfType([_propTypes["default"].bool, _propTypes["default"].array]),
+  loading: _propTypes["default"].bool,
+  onChange: _propTypes["default"].func.isRequired,
+  getOptionLabel: _propTypes["default"].oneOfType([_propTypes["default"].object, _propTypes["default"].func]),
+  getOptionSelected: _propTypes["default"].oneOfType([_propTypes["default"].object, _propTypes["default"].func]),
+  renderOption: _propTypes["default"].oneOfType([_propTypes["default"].object, _propTypes["default"].func])
 };
-var styledSelectComponent = (0, _core.withStyles)(_styles.styles)(SelectComponent);
-exports.Select = styledSelectComponent;
--- src/components/inputs/Select/index.js
+++ src/components/inputs/Select/index.js
@@ -2,9 +2,20 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
+import Chip from '@material-ui/core/Chip';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles, CircularProgress } from '@material-ui/core';
 import { useSafeIntl } from '../../../utils/useSafeIntl';
 import { MESSAGES } from './messages';
+
+const useStyles = makeStyles(() => ({
+    chipLabel: {
+        marginTop: -2,
+    },
+    startAdornment: {
+        marginTop: -5,
+    },
+}));
 
 const SelectCustom = ({
     value,
@@ -22,57 +33,135 @@ const SelectCustom = ({
     noOptionsText,
     getOptionLabel,
     getOptionSelected,
+    loading,
+    renderOption,
 }) => {
-    const [selectedValue, setSelectedValue] = useState(multi ? [] : null);
-    const [search, setSearch] = useState(null);
+    const [selectedValue, setSelectedValue] = useState(null);
+    const [multiSelectedValue, setMultiSelectedValue] = useState([]);
     const intl = useSafeIntl();
+    const classes = useStyles();
+
+    const getOption = optionValue =>
+        options.find(o => `${o.value}` === `${optionValue}`);
 
     useEffect(() => {
-        if (multi && value && Array.isArray(value)) {
-            const newSelectedValue = [];
-            value.forEach(v => {
-                const option = options.find(o => o.id === v);
-                newSelectedValue.push(option);
-            });
-            options.find(o => o.id === value);
-            setSelectedValue(newSelectedValue || []);
-        }
-        if (!multi) {
-            const newSelectedValue = options.find(o => o.id === value);
-            setSelectedValue(newSelectedValue || null);
+        if (value) {
+            if (multi) {
+                const newSelectedValue = [];
+                const valuesList = Array.isArray(value)
+                    ? value
+                    : value.split(',');
+
+                valuesList.forEach(v => {
+                    const option = getOption(v);
+                    if (option) newSelectedValue.push(getOption(v));
+                });
+                setMultiSelectedValue(newSelectedValue);
+            } else {
+                const newSelectedValue = getOption(value);
+                if (newSelectedValue) setSelectedValue(newSelectedValue);
+            }
+        } else {
+            multi ? setMultiSelectedValue([]) : setSelectedValue(null);
         }
     }, [value, options]);
+
+    const handleChange = (e, newValue) => {
+        if ((!multi && !newValue) || (multi && newValue.length === 0)) {
+            return onChange(null);
+        }
+        if (multi) {
+            return onChange(newValue.map(v => v && v.value).join(','));
+        }
+        return onChange(newValue.value);
+    };
+    const extraProps = {
+        getOptionLabel: getOptionLabel || (option => option && option.label),
+        getOptionSelected:
+            getOptionSelected ||
+            ((option, val) => val && option.value === val.value),
+    };
+    if (renderOption) {
+        extraProps.renderOption = renderOption;
+    }
     return (
-        <Box mt={1} mb={3}>
+        <Box mt={1} mb={2}>
             <Autocomplete
                 noOptionsText={intl.formatMessage(noOptionsText)}
                 multiple={multi}
                 id={keyValue}
                 disableClearable={!clearable}
                 options={options}
-                value={selectedValue}
-                onInputChange={(e, newSearch) => setSearch(newSearch)}
-                onChange={(e, newValue) => {
-                    if (multi) {
-                        return onChange(newValue.map(v => v.id));
+                value={multi ? multiSelectedValue : selectedValue}
+                onChange={handleChange}
+                loading={loading}
+                renderTags={(tagValue, getTagProps) =>
+                    tagValue.map((option, index) => {
+                        if (!option) return null;
+                        return (
+                            <Chip
+                                classes={{
+                                    label: classes.chipLabel,
+                                }}
+                                color="secondary"
+                                label={option.label}
+                                {...getTagProps({ index })}
+                            />
+                        );
+                    })
+                }
+                renderInput={params => {
+                    const paramsCopy = {
+                        ...params,
+                    };
+                    let inputExtraProps = {};
+                    if (extraProps.renderOption && params.inputProps.value) {
+                        inputExtraProps = {
+                            startAdornment: (
+                                <div className={classes.startAdornment}>
+                                    {extraProps.renderOption({
+                                        label: params.inputProps.value,
+                                    })}
+                                </div>
+                            ),
+                            style: { color: 'transparent' },
+                        };
+                        inputExtraProps.startAdornment = (
+                            <div className={classes.startAdornment}>
+                                {extraProps.renderOption({
+                                    label: params.inputProps.value,
+                                })}
+                            </div>
+                        );
+                        paramsCopy.inputProps.value = '';
                     }
-                    return onChange(newValue ? newValue.id : null);
+                    return (
+                        <TextField
+                            {...paramsCopy}
+                            variant="outlined"
+                            disabled={disabled}
+                            label={`${label}${required ? '*' : ''}`}
+                            onBlur={onBlur}
+                            error={errors.length > 0 && touched}
+                            InputProps={{
+                                ...params.InputProps,
+                                endAdornment: (
+                                    <>
+                                        {loading ? (
+                                            <CircularProgress
+                                                color="inherit"
+                                                size={20}
+                                            />
+                                        ) : null}
+                                        {params.InputProps.endAdornment}
+                                    </>
+                                ),
+                                ...inputExtraProps,
+                            }}
+                        />
+                    );
                 }}
-                getOptionLabel={getOptionLabel}
-                getOptionSelected={getOptionSelected}
-                renderInput={params => (
-                    <TextField
-                        {...params}
-                        InputLabelProps={{
-                            shrink: Boolean(value) || Boolean(search),
-                        }}
-                        variant="outlined"
-                        disabled={disabled}
-                        label={`${label}${required ? '*' : ''}`}
-                        onBlur={onBlur}
-                        error={errors.length > 0 && touched}
-                    />
-                )}
+                {...extraProps}
             />
         </Box>
     );
@@ -87,11 +176,12 @@ SelectCustom.defaultProps = {
     clearable: true,
     required: false,
     touched: false,
-    searchable: true,
+    loading: false,
     options: [],
     onBlur: () => {},
-    getOptionSelected: (option, value) => option.value === value,
-    getOptionLabel: option => option.label,
+    getOptionSelected: null,
+    getOptionLabel: null,
+    renderOption: null,
     noOptionsText: MESSAGES.noOptions,
 };
 
@@ -101,7 +191,6 @@ SelectCustom.propTypes = {
     label: PropTypes.string,
     required: PropTypes.bool,
     disabled: PropTypes.bool,
-    searchable: PropTypes.bool,
     clearable: PropTypes.bool,
     multi: PropTypes.bool,
     value: PropTypes.any,
@@ -109,9 +198,11 @@ SelectCustom.propTypes = {
     noOptionsText: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
     options: PropTypes.array,
     touched: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
+    loading: PropTypes.bool,
     onChange: PropTypes.func.isRequired,
-    getOptionLabel: PropTypes.func,
-    getOptionSelected: PropTypes.func,
+    getOptionLabel: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    getOptionSelected: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+    renderOption: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
 };
 
 export { SelectCustom as Select };

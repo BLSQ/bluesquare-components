4ea4d5bb993589a5a37fd5be2e810f880def8bde
Christophe Gerard <c@eduz.be>
2022-04-12 15:53:29 +0200

    :fire: create url if value is empty string

--- dist/utils/fetchData.js
+++ dist/utils/fetchData.js
@@ -12,7 +12,7 @@ function createUrl(params) {
   Object.keys(params).forEach(function (key) {
     var value = params[key];
 
-    if (value !== undefined && value !== null) {
+    if (value !== undefined && value !== null && value !== '') {
       url += "/".concat(key, "/").concat(value); // eslint-disable-line
     }
   });

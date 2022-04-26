442e35991b67474c313414161d33364b835ae630
Quang Son Le <qle@bluesquarehub.com>
2022-04-26 17:25:03 +0200

    IA-1032: change compiler option to commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    entry: './index.js',
+    entry: './index.ts',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),

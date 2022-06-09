a87828ee5974b70ef123318da77f05f842e2e30b
axel <tavormina.axel@gmail.com>
2022-06-09 15:37:14 +0200

    refactor RadioComponent : wrap with FormControl

--- src/components/inputs/Radio/index.js
+++ src/components/inputs/Radio/index.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import PropTypes, { oneOfType } from 'prop-types';
-import { RadioGroup, FormControlLabel, Radio } from '@material-ui/core';
+import {
+    RadioGroup,
+    FormControl,
+    FormLabel,
+    FormControlLabel,
+    Radio,
+} from '@material-ui/core';
 
-const RadioComponent = ({ options, name, onChange, value, className }) => (
-    <RadioGroup
-        className={className}
-        name={name}
-        value={value}
-        onChange={event => {
-            onChange(event.target.value);
-        }}
+const RadioComponent = ({
+    options,
+    name,
+    onChange,
+    value,
+    label,
+    errors,
+    className,
+}) => (
+    <FormControl
+        component="fieldset"
+        error={errors.length > 0}
+        variant="outlined"
     >
-        {options.map(o => (
-            <FormControlLabel
-                key={o.value}
-                value={o.value}
-                control={<Radio color="primary" />}
-                label={o.label}
-                disabled={o.disabled}
-            />
-        ))}
-    </RadioGroup>
+        <FormLabel component="legend" style={{ fontSize: 12 }}>
+            {label}
+        </FormLabel>
+
+        <RadioGroup
+            className={className}
+            name={name}
+            value={value}
+            onChange={event => {
+                onChange(event.target.value);
+            }}
+        >
+            {options.map(o => (
+                <FormControlLabel
+                    key={o.value}
+                    value={o.value}
+                    control={<Radio color="primary" />}
+                    label={o.label}
+                    disabled={o.disabled}
+                />
+            ))}
+        </RadioGroup>
+    </FormControl>
 );
 
 RadioComponent.defaultProps = {
     options: [],
     onChange: () => {},
     value: '',
+    label: '',
+    errors: [],
     className: '',
 };
 RadioComponent.propTypes = {
@@ -34,6 +60,8 @@ RadioComponent.propTypes = {
     name: PropTypes.string.isRequired,
     onChange: PropTypes.func,
     value: oneOfType([PropTypes.string, PropTypes.bool]),
+    label: PropTypes.string,
+    errors: PropTypes.arrayOf(PropTypes.string),
     className: PropTypes.string,
 };
 

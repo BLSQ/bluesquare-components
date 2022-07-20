c364611b4961b688875d18dc9956104fd01ca232
Quang Son Le <qle@bluesquarehub.com>
2022-07-20 17:54:16 +0200

    POLIO-453 update error boundary

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true, error };
+    }
+
     componentDidCatch(error, errorInfo) {
-        // eslint-disable-next-line no-console
-        console.log('BOUNDARY ERROR', error, errorInfo);
+        console.error('BOUNDARY ERROR', error, errorInfo);
     }
 
     render() {
-        // eslint-disable-next-line react/prop-types
+        if (this.state.hasError) {
+            return (
+                <h1>An exception occurred: {this.state.error.toString()}</h1>
+            );
+        }
         return this.props.children;
     }
 }
 
+ErrorBoundary.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 export { ErrorBoundary };

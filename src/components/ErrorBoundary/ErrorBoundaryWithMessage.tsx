3661ad4ef914813c00abfe4bffda6ab836847887
Quang Son Le <38907762+quang-le@users.noreply.github.com>
2022-07-25 15:19:46 +0200

    Polio 453 update error boundary (#71)
    * POLIO-453 update error boundary
    
    * POLIO-453: update EB
    
    * POLIO-453: add ErrorBundaryWithMessage
    
    * POLIO-453: add default styling, classNAme props

---
+++ src/components/ErrorBoundary/ErrorBoundaryWithMessage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ErrorBoundary } from '.'
+import { useSafeIntl } from '../../utils/useSafeIntl'
+
+export const ErrorBoundaryWithMessage = (props) => {
+    const {formatMessage} = useSafeIntl()
+    return (
+        <ErrorBoundary
+        message={formatMessage(props.message)}>
+            {props.children}
+        </ErrorBoundary>
+    )
+
+}
--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true, error };
+    }
+
     componentDidCatch(error, errorInfo) {
-        // eslint-disable-next-line no-console
-        console.log('BOUNDARY ERROR', error, errorInfo);
+        console.error('BOUNDARY ERROR', error, errorInfo);
     }
 
     render() {
-        // eslint-disable-next-line react/prop-types
+        if (this.state.hasError) {
+            if (this.props.message) {
+                return (
+                    <h1
+                        style={
+                            this.props.className ? {} : { textAlign: 'center' }
+                        }
+                        className={this.props.className}
+                    >
+                        {this.props.message}
+                    </h1>
+                );
+            }
+            return (
+                <h1>An exception occurred: {this.state.error.toString()}</h1>
+            );
+        }
         return this.props.children;
     }
 }
 
+ErrorBoundary.propTypes = {
+    children: PropTypes.node.isRequired,
+    message: PropTypes.string,
+    className: PropTypes.string,
+};
+ErrorBoundary.defaultProps = {
+    message: null,
+    className: null,
+};
 export { ErrorBoundary };

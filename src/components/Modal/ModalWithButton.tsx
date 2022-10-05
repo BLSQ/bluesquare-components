3cd48987444ea321ffc333067ff0affa07e163d8
Quang Son Le <qle@bluesquarehub.com>
2022-10-05 17:22:13 +0200

    POLIO-619: enable modal to be open by default

---
+++ src/components/Modal/ModalWithButton.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { ComponentType, FunctionComponent, useState } from 'react';
+
+type ModalComponentProps = { closeDialog: () => void; isOpen: boolean };
+type ButtonComponentProps = { onClick: () => void };
+
+type ModalProps<T extends ModalComponentProps> = Omit<
+    T,
+    'closeDialog' | 'isOpen'
+>;
+type ButtonProps<T extends ButtonComponentProps> = Omit<T, 'onClick'>;
+
+type FullModalProps<
+    T extends ModalComponentProps,
+    U extends ButtonComponentProps,
+> = ModalProps<T> & {
+    defaultOpen?:boolean;
+    iconProps: ButtonProps<U>;
+};
+
+export const makeFullModal =
+    <T extends ModalComponentProps, U extends ButtonComponentProps>(
+        ModalComponent: ComponentType<T>,
+        ButtonComponent: ComponentType<U>,
+    ): FunctionComponent<FullModalProps<T, U>> =>
+    (props: FullModalProps<T, U>) => {
+        const { iconProps, defaultOpen, ...modalProps } = props;
+        const [openModal, setOpenModal] = useState<boolean>(defaultOpen ?? false);
+        return (
+            <>
+                <ButtonComponent
+                    {...({
+                        ...iconProps,
+                        onClick: () => setOpenModal(true),
+                    } as U)}
+                />
+                {openModal && (
+                    <ModalComponent
+                        {...({
+                            ...modalProps,
+                            closeDialog: () => setOpenModal(false),
+                            isOpen: openModal,
+                        } as unknown as T)}
+                    />
+                )}
+            </>
+        );
+    };

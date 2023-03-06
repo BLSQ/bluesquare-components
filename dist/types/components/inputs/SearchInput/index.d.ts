9c0511520ee615a92594c135a1253679edb328cb
Axel Tavormina <tavormina.axel@gmail.com>
2023-03-06 17:37:06 +0100

    build

---
+++ dist/types/components/inputs/SearchInput/index.d.ts
@@ -0,0 +1,18 @@
+import { FunctionComponent } from 'react';
+declare type Props = {
+    uid: string;
+    label: string;
+    keyValue: string;
+    required?: boolean;
+    disabled?: boolean;
+    clearable?: boolean;
+    onEnterPressed: () => void;
+    onChange: (newValue: string) => void;
+    onErrorChange: (hasError: boolean) => void;
+    blockForbiddenChars: boolean;
+    value: string;
+    errors: [];
+    autoComplete: string;
+};
+declare const SearchInput: FunctionComponent<Props>;
+export { SearchInput };
---
+++ dist/types/utils/containsForbiddenChars.d.ts
@@ -0,0 +1,1 @@
+export declare const containsForbiddenCharacter: (value: string, forbiddenCharacters: string[]) => boolean;

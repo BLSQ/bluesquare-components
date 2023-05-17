21d83841d0acf0f6010484ba31b7b728556ba2d2
beygorghor <cgerard@bluesquarehub.com>
2023-05-17 15:47:33 +0200

    header can also be a react element (#120)

--- src/components/table/InfoHeader/styles.js
+++ src/components/table/InfoHeader/styles.js
@@ -3,14 +3,6 @@ import { commonStyles } from '../../../styles/iaso/common';
 
 const styles = theme => ({
     ...commonStyles(theme),
-    root: {
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
-        top: 0,
-        left: 0,
-        overflow: 'hidden',
-    },
     icon: {
         cursor: 'pointer',
         position: 'relative',

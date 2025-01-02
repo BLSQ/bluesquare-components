ffeea8e20e117478b8c25e8b6733c5193b346148
Quang Son Le <qle@bluesquarehub.com>
2025-01-02 18:00:32 +0100

    IA-3063: build

--- dist/types/components/DateTimePicker/index.d.ts
+++ dist/types/components/DateTimePicker/index.d.ts
@@ -1,4 +1,4 @@
-export function DateTimePicker({ label, onChange, currentDate, clearMessage, required, errors, hideError, disabled, clearable, }: {
+export function DateTimePicker({ label, onChange, currentDate, clearMessage, required, errors, hideError, disabled, clearable, maxTime, maxDateTime, maxDate, minTime, minDateTime, minDate, }: {
     label: any;
     onChange: any;
     currentDate: any;
@@ -8,6 +8,12 @@ export function DateTimePicker({ label, onChange, currentDate, clearMessage, req
     hideError: any;
     disabled: any;
     clearable: any;
+    maxTime: any;
+    maxDateTime: any;
+    maxDate: any;
+    minTime: any;
+    minDateTime: any;
+    minDate: any;
 }): React.JSX.Element;
 export namespace DateTimePicker {
     namespace defaultProps {
@@ -18,6 +24,12 @@ export namespace DateTimePicker {
         const disabled: boolean;
         const clearMessage: undefined;
         const clearable: boolean;
+        const maxTime: undefined;
+        const maxDateTime: undefined;
+        const maxDate: undefined;
+        const minTime: undefined;
+        const minDateTime: undefined;
+        const minDate: undefined;
     }
     namespace propTypes {
         export const label: PropTypes.Validator<string>;
@@ -36,6 +48,18 @@ export namespace DateTimePicker {
         export { disabled_1 as disabled };
         const clearable_1: PropTypes.Requireable<boolean>;
         export { clearable_1 as clearable };
+        const maxTime_1: PropTypes.Requireable<any>;
+        export { maxTime_1 as maxTime };
+        const maxDateTime_1: PropTypes.Requireable<any>;
+        export { maxDateTime_1 as maxDateTime };
+        const maxDate_1: PropTypes.Requireable<any>;
+        export { maxDate_1 as maxDate };
+        const minTime_1: PropTypes.Requireable<any>;
+        export { minTime_1 as minTime };
+        const minDateTime_1: PropTypes.Requireable<any>;
+        export { minDateTime_1 as minDateTime };
+        const minDate_1: PropTypes.Requireable<any>;
+        export { minDate_1 as minDate };
     }
 }
 import React from "react";

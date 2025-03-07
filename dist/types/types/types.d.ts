b44bb16499972029b2ee62c1678a71840dcde9e1
Quang Son Le <qle@bluesquarehub.com>
2025-03-07 13:57:12 +0100

    GAPS-79: export dropdown type

--- dist/types/types/types.d.ts
+++ dist/types/types/types.d.ts
@@ -7,3 +7,27 @@ export type IntlFormatMessage = (message: IntlMessage, values?: Record<string, s
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 export type Nullable<T> = T | null;
 export type Optional<T> = T | undefined;
+export type DropdownOptions<T> = {
+    label: string;
+    value: T;
+    original?: Record<any, any>;
+    color?: string;
+};
+export type DropdownOptionsWithOriginal<T, O = Record<any, any>> = {
+    label: string;
+    value: T;
+    original: O;
+};
+export type ValidationError = Record<string, string> | null | undefined;
+export type GenericObject = Record<string, any>;
+export type ClassNames = Record<string, string>;
+export type NameAndId = {
+    name: string;
+    id: number;
+};
+export type RequireOnlyOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> & {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>;
+}[Keys];
+export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> & {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+}[Keys];

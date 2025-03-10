c23ee2ee8274838e706e077d0089dbe7fd7f145c
Quang Son Le <qle@bluesquarehub.com>
2025-03-10 22:37:19 +0100

    GAPS-79: enable non int values in CheckBoxSelectGroup

--- dist/types/components/inputs/CheckBoxSelectGroup/CheckBoxSelectGroup.d.ts
+++ dist/types/components/inputs/CheckBoxSelectGroup/CheckBoxSelectGroup.d.ts
@@ -7,6 +7,7 @@ type Props = {
     filterState: FilterState;
     defaultItemCount?: number;
     title?: string;
+    valueIsString?: boolean;
 };
 /** A group of checkboxes that constitute a filter akin to a SelectInput. State and options are received from the parent. Each CheckBoxSelectGroup constitues one filter,
  * i.e one entry in a `FilterState.filters` object.
@@ -14,6 +15,7 @@ type Props = {
  * @param title {string} - Title to be displayed above the group of checkboxes. Typically the filter name
  * @param keyValue {string} - the key used to retrieve the value in a FilterState.filters object
  * @param filterState {FilterState} - The parent filter's state. Typically generated using `useFilterState`
+ * @param valueIsString {boolean} - set to true if the `value`of the options is not a number or can't be parsed with parseInt
  * @param options {DropdownOptions<number>[]} - The list of possible values for the filter. Each element in the list corresponds to a checkbox
  * @param isLoading {boolean} - Can be used when options are fetched asynchronously to indicate data is being loaded
  * @param defaultItemCount (number) - The amount of items to show when `options`has many elements. If options.length > defaultItemCount, a "show more" button will be displayed. Defaults to 5
--- src/components/inputs/CheckBoxSelectGroup/CheckBoxSelectGroup.tsx
+++ src/components/inputs/CheckBoxSelectGroup/CheckBoxSelectGroup.tsx
@@ -16,6 +16,7 @@ type Props = {
     filterState: FilterState;
     defaultItemCount?: number;
     title?: string;
+    valueIsString?: boolean;
 };
 
 /** A group of checkboxes that constitute a filter akin to a SelectInput. State and options are received from the parent. Each CheckBoxSelectGroup constitues one filter,
@@ -24,6 +25,7 @@ type Props = {
  * @param title {string} - Title to be displayed above the group of checkboxes. Typically the filter name
  * @param keyValue {string} - the key used to retrieve the value in a FilterState.filters object
  * @param filterState {FilterState} - The parent filter's state. Typically generated using `useFilterState`
+ * @param valueIsString {boolean} - set to true if the `value`of the options is not a number or can't be parsed with parseInt
  * @param options {DropdownOptions<number>[]} - The list of possible values for the filter. Each element in the list corresponds to a checkbox
  * @param isLoading {boolean} - Can be used when options are fetched asynchronously to indicate data is being loaded
  * @param defaultItemCount (number) - The amount of items to show when `options`has many elements. If options.length > defaultItemCount, a "show more" button will be displayed. Defaults to 5
@@ -35,6 +37,7 @@ export const CheckBoxSelectGroup: FunctionComponent<Props> = ({
     isLoading,
     filterState,
     title,
+    valueIsString = false,
     defaultItemCount = 5,
 }) => {
     const { formatMessage } = useSafeIntl();
@@ -66,6 +69,7 @@ export const CheckBoxSelectGroup: FunctionComponent<Props> = ({
                                 disabled={isLoading}
                                 option={option}
                                 filterState={filterState}
+                                valueIsString={valueIsString}
                             />
                         </Grid>
                     );

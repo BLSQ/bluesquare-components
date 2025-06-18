be992516201fb9539ef8c2185e07010ef9ab10aa
Bewi <benjamin.wilfart@gmail.com>
2025-06-18 13:42:12 +0200

    Query Builder: Add missing props and build

--- dist/types/components/QueryBuilder/components/QueryBuilder.d.ts
+++ dist/types/components/QueryBuilder/components/QueryBuilder.d.ts
@@ -1,8 +1,11 @@
 import { FunctionComponent } from 'react';
-import { JsonLogicTree, Fields } from '@react-awesome-query-builder/mui';
+import { JsonLogicTree, Fields, Settings, Conjunctions, Operators } from '@react-awesome-query-builder/mui';
 type Props = {
     logic?: JsonLogicTree;
     fields: Fields;
+    settings?: Settings;
+    conjunctions?: Conjunctions;
+    operators?: Operators;
     onChange: (jsonLogic: JsonLogicTree) => void;
 };
 export declare const QueryBuilder: FunctionComponent<Props>;

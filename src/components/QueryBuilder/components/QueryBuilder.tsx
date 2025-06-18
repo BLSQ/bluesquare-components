d34da231dd521803bcea543c0f40e0b630762e85
Bewi <benjamin.wilfart@gmail.com>
2025-06-18 16:28:40 +0200

    Query builder: Add some documentation

--- src/components/QueryBuilder/components/QueryBuilder.tsx
+++ src/components/QueryBuilder/components/QueryBuilder.tsx
@@ -38,6 +38,18 @@ type Props = {
 
 const queryValue: JsonGroup = { id: QbUtils.uuid(), type: 'group' };
 
+/**
+ * QueryBuilder component for building queries using a visual interface.
+ * It allows users to create complex queries with fields, conjunctions, and operators.
+ *
+ * @param {Object} props - The properties for the QueryBuilder component.
+ * @param {JsonLogicTree} [props.logic] - Initial logic for the query.
+ * @param {Fields} props.fields - Fields available for building the query.
+ * @param {Settings} [props.settings] - Settings for the query builder.
+ * @param {Conjunctions} [props.conjunctions] - Conjunctions available in the query builder. This won't be translated by the component, meaning that this prop should contain the translated conjunctions.
+ * @param {Operators} [props.operators] - Operators available in the query builder. This won't be translated by the component, meaning that this prop should contain the translated operators.
+ * @param {Function} props.onChange - Callback function to handle changes in the query.
+ */
 export const QueryBuilder: FunctionComponent<Props> = ({
     logic,
     fields,

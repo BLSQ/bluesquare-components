16dac2172f8698a0026c4791c51a1588735d49ee
Quang Son Le <qle@bluesquarehub.com>
2025-12-29 18:05:33 +0100

    build

--- dist/types/components/buttons/AddButton/index.d.ts
+++ dist/types/components/buttons/AddButton/index.d.ts
@@ -1,42 +1,14 @@
-export { styledAddButton as AddButton };
-declare const styledAddButton: React.JSXElementConstructor<Omit<Pick<{
-    classes: any;
+import { FunctionComponent } from 'react';
+import { IntlMessage } from '../../../types/types';
+type Props = {
     onClick: any;
-    message?: {
-        id: string;
-        defaultMessage: string;
-    } | undefined;
-    id?: string | undefined;
-    dataTestId?: string | undefined;
-    size?: string | undefined;
-    disabled?: boolean | undefined;
-}, "disabled" | "id" | "size" | "message" | "dataTestId"> & Pick<PropTypes.InferProps<{
-    classes: PropTypes.Validator<object>;
-    onClick: PropTypes.Validator<(...args: any[]) => any>;
-    message: PropTypes.Requireable<object>;
-    id: PropTypes.Requireable<string>;
-    dataTestId: PropTypes.Requireable<string>;
-    size: PropTypes.Requireable<string>;
-    disabled: PropTypes.Requireable<boolean>;
-}>, PropTypes.RequiredKeys<{
-    classes: PropTypes.Validator<object>;
-    onClick: PropTypes.Validator<(...args: any[]) => any>;
-    message: PropTypes.Requireable<object>;
-    id: PropTypes.Requireable<string>;
-    dataTestId: PropTypes.Requireable<string>;
-    size: PropTypes.Requireable<string>;
-    disabled: PropTypes.Requireable<boolean>;
-}>> & Pick<{
-    classes: any;
-    onClick: any;
-    message?: {
-        id: string;
-        defaultMessage: string;
-    } | undefined;
-    id?: string | undefined;
-    dataTestId?: string | undefined;
-    size?: string | undefined;
-    disabled?: boolean | undefined;
-}, never>, "classes"> & import("@mui/styles").StyledComponentProps<string> & object>;
-import PropTypes from 'prop-types';
-import React from 'react';
+    message?: IntlMessage;
+    id?: string;
+    dataTestId?: string;
+    size?: 'medium' | 'large' | 'small' | undefined;
+    variant?: 'text' | 'contained' | 'outlined';
+    disabled?: boolean;
+    color?: 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning';
+};
+export declare const AddButton: FunctionComponent<Props>;
+export {};

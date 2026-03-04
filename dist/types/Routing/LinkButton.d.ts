ca27752b251bd973a2a971eaa78175f66fa32015
Crebert08 <benicrebertbaransegeta@gmail.com>
2026-03-04 14:28:54 +0200

    LinkWithButton: dist

--- dist/types/Routing/LinkButton.d.ts
+++ dist/types/Routing/LinkButton.d.ts
@@ -1,10 +1,13 @@
 import { ButtonProps } from '@mui/material';
 import { FunctionComponent, ReactNode } from 'react';
 import { LinkProps } from 'react-router-dom';
-type Props = LinkProps & ButtonProps & {
+type Props = LinkProps & {
+    disabled?: boolean;
+} & ButtonProps & {
     children: ReactNode;
     className?: string;
     buttonClassName?: string;
+    unstable_viewTransition?: any;
 };
 export declare const LinkButton: FunctionComponent<Props>;
 export {};

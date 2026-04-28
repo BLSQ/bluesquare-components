a61daa4ec550a6b7100d2fa9eca895f17f4c1cfd
quang-le <qle@bluesquarehub.com>
2026-04-28 09:56:25 +0200

    chore: fix build

--- dist/types/components/buttons/AddButton/index.d.ts
+++ dist/types/components/buttons/AddButton/index.d.ts
@@ -2,46 +2,6 @@ import { FunctionComponent } from 'react';
 import { IntlMessage } from '../../../types/types';
 type Props = {
     onClick: any;
-<<<<<<< HEAD
-    message: any;
-    id: any;
-    dataTestId: any;
-    size: any;
-    disabled: any;
-}, never> & Pick<PropTypes.InferProps<{
-    classes: PropTypes.Validator<object>;
-    onClick: PropTypes.Validator<(...args: any[]) => any>;
-    message: PropTypes.Requireable<object>;
-    id: PropTypes.Requireable<string>;
-    dataTestId: PropTypes.Requireable<string>;
-    size: PropTypes.Requireable<string>;
-    disabled: PropTypes.Requireable<boolean>;
-}>, "disabled" | "id" | "size" | "message" | "dataTestId" | PropTypes.RequiredKeys<{
-    classes: PropTypes.Validator<object>;
-    onClick: PropTypes.Validator<(...args: any[]) => any>;
-    message: PropTypes.Requireable<object>;
-    id: PropTypes.Requireable<string>;
-    dataTestId: PropTypes.Requireable<string>;
-    size: PropTypes.Requireable<string>;
-    disabled: PropTypes.Requireable<boolean>;
-}>>, PropTypes.RequiredKeys<{
-    classes: PropTypes.Validator<object>;
-    onClick: PropTypes.Validator<(...args: any[]) => any>;
-    message: PropTypes.Requireable<object>;
-    id: PropTypes.Requireable<string>;
-    dataTestId: PropTypes.Requireable<string>;
-    size: PropTypes.Requireable<string>;
-    disabled: PropTypes.Requireable<boolean>;
-}>> & {
-    disabled?: boolean | null | undefined;
-    id?: string | null | undefined;
-    size?: string | null | undefined;
-    message?: object | null | undefined;
-    dataTestId?: string | null | undefined;
-} & {}, "classes"> & import("@mui/styles").StyledComponentProps<string> & object>;
-import PropTypes from 'prop-types';
-import React from 'react';
-=======
     message?: IntlMessage;
     id?: string;
     dataTestId?: string;
@@ -52,4 +12,3 @@ import React from 'react';
 };
 export declare const AddButton: FunctionComponent<Props>;
 export {};
->>>>>>> IA-4640_safe_date_picker
